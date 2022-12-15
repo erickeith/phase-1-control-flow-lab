a23a72c523140884719516d7777cd971b14ece81
Eric <eric.keith@flatironschool.com>
2022-12-15 13:29:16 -0600

    commit tree 05d2555d2d31a5ee2b2b1a90f90fd5ec9f5e57d5

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!"
+  } else if (distance > 400 && distance <= 2000) {
+    return "That will be twenty bucks."
+  } else if (distance > 2000 && distance < 2501) {
+    return "I will gladly take your thirty bucks."
+  } else { return "No can do."}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city == "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(message){
   // Write your code here!
+  switch (message) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    case "thanks for everything":
+      return "Bye."
+      break;
+  }
 }
